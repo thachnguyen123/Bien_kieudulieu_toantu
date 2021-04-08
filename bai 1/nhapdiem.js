c676d79408cfd0720ffec3aa163a647771cec52d
thachnguyen123 <40359637+thachnguyen123@users.noreply.github.com>
2021-04-08 13:00:35 +0700

    bai1

---
+++ bai 1/nhapdiem.js
@@ -0,0 +1,9 @@
+let a = +prompt("diem vat li:");
+let b = +prompt("diem vat hoa hoc:");
+let c = +prompt("diem toan:");
+diemtrungbinh = (a+b+c)/3;
+tong = a+b+c;
+{
+    alert("diem trung binh:" + diemtrungbinh);
+}
+alert("tổng điểm:" + tong);
